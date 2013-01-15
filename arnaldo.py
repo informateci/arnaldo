a390ac7185e6c4afe8cf33b6939338ac1022a160
Gaggo <gaggozzo@gmail.com>
2013-01-15 14:07:25 +0100

    Update arnaldo.py
    Added "Parliamo di <wikipedia random page title>"

--- arnaldo.py
+++ arnaldo.py
@@ -2,6 +2,8 @@ from minibot import MiniBot
 import pycurl
 import cStringIO
 from random import choice, randint
+import urllib2
+import json
 
 
 class GourmetBot(MiniBot):
@@ -10,12 +12,25 @@ class GourmetBot(MiniBot):
         self.cy = file('SUB-EST2011-01.csv', 'r').read()
         self.nn = file('nounlist.txt', 'r').read()
         MiniBot.__init__(self, 'chat.freenode.net', 6666, '#informateci', 'Illuvatar594')
+        opener = urllib2.build_opener()
+    	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
     def _on_message(self, author, content, private):
-        if randint(0, 2000) <= 2 or ("ANAL" in content):
+        if ("ANAL" in content):
             print "Say it!"
             sayit=self._doit()
             self.write_message(sayit)
+        else
+			nrand = randint(0, 2000)
+            if nrand <= 2:
+				print "Say it!"
+				sayit=self._doit()
+				self.write_message(sayit)
+			elif nrand <= 4:
+				print "Say it!"
+				infile = opener.open('http://it.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json')
+				sayit="Parliamo di " + json.loads(infile.read())['query']['random'][0]['title']
+				self.write_message(sayit)
             
     def _doit(self):
         citta=choice([[a.split(',')[1] for a in (self.cy).split(",,,,")[6:-11]]][0])
@@ -36,3 +51,10 @@ if __name__ == "__main__":
     bot = GourmetBot()
     bot.register_command("tipo?", bot._tipo)
     bot.start()
+
+
+
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+infile = opener.open('http://it.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json')
+title = json.loads(infile.read())['query']['random'][0]['title']
