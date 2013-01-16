86076548583b0eccf154c62ee257c0fd80eee0cc
autoscatto <4utoscatto@gmail.com>
2013-01-16 16:11:51 +0100

    pete nerissime

--- arnaldo.py
+++ arnaldo.py
@@ -2,7 +2,7 @@ from minibot import MiniBot
 import pycurl
 import cStringIO
 from random import choice, randint
-import urllib2
+import requests
 import json
 
 
@@ -12,15 +12,13 @@ class GourmetBot(MiniBot):
         self.cy = file('SUB-EST2011-01.csv', 'r').read()
         self.nn = file('nounlist.txt', 'r').read()
         MiniBot.__init__(self, 'chat.freenode.net', 6666, '#informateci', 'Illuvatar594')
-        opener = urllib2.build_opener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
     def _on_message(self, author, content, private):
         if ("ANAL" in content):
             print "Say it!"
             sayit=self._doit()
             self.write_message(sayit)
-        else
+        else:
             nrand = randint(0, 2000)
             if nrand <= 2:
                 print "Say it!"
@@ -28,10 +26,13 @@ class GourmetBot(MiniBot):
                 self.write_message(sayit)
             elif nrand <= 4:
                 print "Say it!"
-                infile = opener.open('http://it.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json')
-                sayit="Parliamo di " + json.loads(infile.read())['query']['random'][0]['title']
-                self.write_message(sayit)
-            
+                respa = requests.get('http://it.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json', headers={'User-Agent':'Mozilla/5.0'})
+                if(respa.ok):
+                    msgg=reduce(dict.get,['query','random'], json.loads(respa.content))
+                    if(msgg is not None and len(msgg)>0 and msgg[0].get('title',None) is not None):
+                        sayit="Parliamo di " + msgg[0].get('title',None)
+                        self.write_message(sayit)
+
     def _doit(self):
         citta=choice([[a.split(',')[1] for a in (self.cy).split(",,,,")[6:-11]]][0])
         citta=citta.upper()
