893dd74ba3b0e6e802be9ff23cce685ab0879746
autoscatto <4utoscatto@gmail.com>
2013-10-29 13:47:03 +0100

    ICS ICS ICS

--- arnaldo.py
+++ arnaldo.py
@@ -54,109 +54,14 @@ traceback_template = '''Tracefazza (most recent call last):
 
 URL_RE = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
 
-letterforms = '''\
-       |       |       |       |       |       |       | |
-  XXX  |  XXX  |  XXX  |   X   |       |  XXX  |  XXX  |!|
-  X  X |  X  X |  X  X |       |       |       |       |"|
-  X X  |  X X  |XXXXXXX|  X X  |XXXXXXX|  X X  |  X X  |#|
- XXXXX |X  X  X|X  X   | XXXXX |   X  X|X  X  X| XXXXX |$|
-XXX   X|X X  X |XXX X  |   X   |  X XXX| X  X X|X   XXX|%|
-  XX   | X  X  |  XX   | XXX   |X   X X|X    X | XXX  X|&|
-  XXX  |  XXX  |   X   |  X    |       |       |       |'|
-   XX  |  X    | X     | X     | X     |  X    |   XX  |(|
-  XX   |    X  |     X |     X |     X |    X  |  XX   |)|
-       | X   X |  X X  |XXXXXXX|  X X  | X   X |       |*|
-       |   X   |   X   | XXXXX |   X   |   X   |       |+|
-       |       |       |  XXX  |  XXX  |   X   |  X    |,|
-       |       |       | XXXXX |       |       |       |-|
-       |       |       |       |  XXX  |  XXX  |  XXX  |.|
-      X|     X |    X  |   X   |  X    | X     |X      |/|
-  XXX  | X   X |X     X|X     X|X     X| X   X |  XXX  |0|
-   X   |  XX   | X X   |   X   |   X   |   X   | XXXXX |1|
- XXXXX |X     X|      X| XXXXX |X      |X      |XXXXXXX|2|
- XXXXX |X     X|      X| XXXXX |      X|X     X| XXXXX |3|
-X      |X    X |X    X |X    X |XXXXXXX|     X |     X |4|
-XXXXXXX|X      |X      |XXXXXX |      X|X     X| XXXXX |5|
- XXXXX |X     X|X      |XXXXXX |X     X|X     X| XXXXX |6|
-XXXXXX |X    X |    X  |   X   |  X    |  X    |  X    |7|
- XXXXX |X     X|X     X| XXXXX |X     X|X     X| XXXXX |8|
- XXXXX |X     X|X     X| XXXXXX|      X|X     X| XXXXX |9|
-   X   |  XXX  |   X   |       |   X   |  XXX  |   X   |:|
-  XXX  |  XXX  |       |  XXX  |  XXX  |   X   |  X    |;|
-    X  |   X   |  X    | X     |  X    |   X   |    X  |<|
-       |       |XXXXXXX|       |XXXXXXX|       |       |=|
-  X    |   X   |    X  |     X |    X  |   X   |  X    |>|
- XXXXX |X     X|      X|   XXX |   X   |       |   X   |?|
- XXXXX |X     X|X XXX X|X XXX X|X XXXX |X      | XXXXX |@|
-   X   |  X X  | X   X |X     X|XXXXXXX|X     X|X     X|A|
-XXXXXX |X     X|X     X|XXXXXX |X     X|X     X|XXXXXX |B|
- XXXXX |X     X|X      |X      |X      |X     X| XXXXX |C|
-XXXXXX |X     X|X     X|X     X|X     X|X     X|XXXXXX |D|
-XXXXXXX|X      |X      |XXXXX  |X      |X      |XXXXXXX|E|
-XXXXXXX|X      |X      |XXXXX  |X      |X      |X      |F|
- XXXXX |X     X|X      |X  XXXX|X     X|X     X| XXXXX |G|
-X     X|X     X|X     X|XXXXXXX|X     X|X     X|X     X|H|
-  XXX  |   X   |   X   |   X   |   X   |   X   |  XXX  |I|
-      X|      X|      X|      X|X     X|X     X| XXXXX |J|
-X    X |X   X  |X  X   |XXX    |X  X   |X   X  |X    X |K|
-X      |X      |X      |X      |X      |X      |XXXXXXX|L|
-X     X|XX   XX|X X X X|X  X  X|X     X|X     X|X     X|M|
-X     X|XX    X|X X   X|X  X  X|X   X X|X    XX|X     X|N|
-XXXXXXX|X     X|X     X|X     X|X     X|X     X|XXXXXXX|O|
-XXXXXX |X     X|X     X|XXXXXX |X      |X      |X      |P|
- XXXXX |X     X|X     X|X     X|X   X X|X    X | XXXX X|Q|
-XXXXXX |X     X|X     X|XXXXXX |X   X  |X    X |X     X|R|
- XXXXX |X     X|X      | XXXXX |      X|X     X| XXXXX |S|
-XXXXXXX|   X   |   X   |   X   |   X   |   X   |   X   |T|
-X     X|X     X|X     X|X     X|X     X|X     X| XXXXX |U|
-X     X|X     X|X     X|X     X| X   X |  X X  |   X   |V|
-X     X|X  X  X|X  X  X|X  X  X|X  X  X|X  X  X| XX XX |W|
-X     X| X   X |  X X  |   X   |  X X  | X   X |X     X|X|
-X     X| X   X |  X X  |   X   |   X   |   X   |   X   |Y|
-XXXXXXX|     X |    X  |   X   |  X    | X     |XXXXXXX|Z|
- XXXXX | X     | X     | X     | X     | X     | XXXXX |[|
-X      | X     |  X    |   X   |    X  |     X |      X|\|
- XXXXX |     X |     X |     X |     X |     X | XXXXX |]|
-   X   |  X X  | X   X |       |       |       |       |^|
-       |       |       |       |       |       |XXXXXXX|_|
-       |  XXX  |  XXX  |   X   |    X  |       |       |`|
-       |   XX  |  X  X | X    X| XXXXXX| X    X| X    X|a|
-       | XXXXX | X    X| XXXXX | X    X| X    X| XXXXX |b|
-       |  XXXX | X    X| X     | X     | X    X|  XXXX |c|
-       | XXXXX | X    X| X    X| X    X| X    X| XXXXX |d|
-       | XXXXXX| X     | XXXXX | X     | X     | XXXXXX|e|
-       | XXXXXX| X     | XXXXX | X     | X     | X     |f|
-       |  XXXX | X    X| X     | X  XXX| X    X|  XXXX |g|
-       | X    X| X    X| XXXXXX| X    X| X    X| X    X|h|
-       |    X  |    X  |    X  |    X  |    X  |    X  |i|
-       |      X|      X|      X|      X| X    X|  XXXX |j|
-       | X    X| X   X | XXXX  | X  X  | X   X | X    X|k|
-       | X     | X     | X     | X     | X     | XXXXXX|l|
-       | X    X| XX  XX| X XX X| X    X| X    X| X    X|m|
-       | X    X| XX   X| X X  X| X  X X| X   XX| X    X|n|
-       |  XXXX | X    X| X    X| X    X| X    X|  XXXX |o|
-       | XXXXX | X    X| X    X| XXXXX | X     | X     |p|
-       |  XXXX | X    X| X    X| X  X X| X   X |  XXX X|q|
-       | XXXXX | X    X| X    X| XXXXX | X   X | X    X|r|
-       |  XXXX | X     |  XXXX |      X| X    X|  XXXX |s|
-       |  XXXXX|    X  |    X  |    X  |    X  |    X  |t|
-       | X    X| X    X| X    X| X    X| X    X|  XXXX |u|
-       | X    X| X    X| X    X| X    X|  X  X |   XX  |v|
-       | X    X| X    X| X    X| X XX X| XX  XX| X    X|w|
-       | X    X|  X  X |   XX  |   XX  |  X  X | X    X|x|
-       |  X   X|   X X |    X  |    X  |    X  |    X  |y|
-       | XXXXXX|     X |    X  |   X   |  X    | XXXXXX|z|
-  XXX  | X     | X     |XX     | X     | X     |  XXX  |{|
-   X   |   X   |   X   |       |   X   |   X   |   X   |||
-  XXX  |     X |     X |     XX|     X |     X |  XXX  |}|
- XX    |X  X  X|    XX |       |       |       |       |~|
-'''.splitlines()
-
-ASCIItable = {}
-for form in letterforms:
-    if '|' in form:
-        ASCIItable[form[-2]] = form[:-3].split('|')
-ROWS = len(ASCIItable.values()[0])
+def pritaicsa(text):
+    icsa=""
+    for row in range(brain.get("asciitable:rows")):
+        for c in text:
+            icsa=str(icsa)+str(brain.lindex("asciitable:%s"%c,row))
+        icsa=icsa+'\n'
+    icsa=icsa+'\n'
+    return icsa
 
 def tdecode(bytes):
     try:
@@ -310,9 +215,6 @@ class BambaRosaNasaBuffer(LineBuffer): #decoda a naso. VIVA!
                     l = ""
             yield l
 
-
-
-
 class Arnaldo(irc.bot.SingleServerIRCBot):
 
     def __init__(self, channel, nickname, server, port=6667):
@@ -451,6 +353,8 @@ class Arnaldo(irc.bot.SingleServerIRCBot):
         else:
             self.connection.privmsg(target, m)
 
+
+
     def icsah(self,e,match):
         try:
             ggallin=None;
@@ -458,24 +362,15 @@ class Arnaldo(irc.bot.SingleServerIRCBot):
                 ggallin=match.groups()[0]
             except:
                 pass
+
             if ggallin:
-                icsa=""
-                for row in range(ROWS):
-                    for c in ggallin:
-                        icsa=str(icsa)+str(ASCIItable[c][row])
-                    icsa=icsa+'\n'
-                icsa=icsa+'\n'
+                icsa= pritaicsa(ggallin)
                 self.reply(e,icsa)
         except:
             pass
 
     def rosa(self, e, match):
-        icsa=""
-        for row in range(ROWS):
-            for c in 'rosa':
-                icsa=str(icsa)+str(ASCIItable[c][row])
-            icsa=icsa+'\n'
-        icsa=icsa+'\n'
+        icsa=ritaicsa("rosa")
         self.reply(e,icsa)
 
     def anal(self, e, match):
