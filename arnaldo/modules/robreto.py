255a76e041e2ef89702536f6a79737f2f585aa3d
mandrake <wandrake@gmail.com>
2015-05-14 20:33:42 +0200

    aranldo

--- arnaldo/modules/robreto.py
+++ arnaldo/modules/robreto.py
@@ -1,15 +1,55 @@
 from arnaldo.modules import Arnaldigno, comanda
-from random import shuffle
+import re
+import random
 
 
 class Robreto(Arnaldigno):
 
     @comanda('(.*robreto.*)')
     def robreto(self, e, match):
-        words = [list(m) for m in match.groups()[0].split() if len(m) > 3 and m != 'robreto']
-        for w in range(0, len(words)):
-            shuffle(words[w], lambda: 0.10)
+        vow = "aeiouy"
+        con = "bcdfghjklmnpqrstvwxz"
+        cv_pattern = "(([%s])([%s]))" % (con, vow)
+        cvc_pattern = "(([%s])([%s])([%s]))" % (con, vow, con)
 
-        self.r(e, ' '.join([''.join(w) for w in words]))
+        tokenamelo = ["(robreto)", "([0-9A-Za-z]+)", "(\s+)", "(.)"]
+        la_stringa = match.groups()[0]
+        al_stingra = ""
+
+        while la_stringa:
+            m = [x for x in [re.match("^" + token, la_stringa) for token in tokenamelo] if x]
+            wut = m[0].groups()[0]
+            la_stringa = la_stringa[len(wut):]
+            if wut == 'robreto':
+                la_stringa = la_stringa.lstrip()
+            else:
+                moneta = random.random()
+                cvs = re.findall(cv_pattern if (moneta > 0.5) else cvc_pattern , wut)
+                if cvs:
+                    idx = random.randrange(0, len(cvs))
+                    rob = cvs[idx][2] + cvs[idx][1] if (moneta > 0.5) else cvs[idx][3] + cvs[idx][2] + cvs[idx][1]
+                    al_stingra += re.sub(cvs[idx][0], rob, wut, 1)
+                else:
+                    al_stingra += wut
+
+        self.r(e, al_stingra)
 
         return True
+
+
+if __name__ == '__main__':
+    def printamela(x):
+        print x
+
+    class Coso(object):
+        def groups(self):
+            return ["messaggio per fare robreto"]
+
+        def register_command(self, x, y):
+            pass
+
+        def reply(self, x, y):
+            print y
+
+    c = Robreto(Coso())
+    c.robreto(printamela, Coso())
