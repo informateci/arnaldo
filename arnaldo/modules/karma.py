803df5f9100f549cd4a2b1818ebf0204b71b22b4
mandrake <wandrake@gmail.com>
2015-05-15 13:23:35 +0200

    più nick, meno cani, Karmelo a tutto tondo

--- arnaldo/modules/karma.py
+++ arnaldo/modules/karma.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from arnaldo.brain import brain
-from arnaldo.conf import NICK, CHAN
+from arnaldo.conf import NICK
 from arnaldo.modules import Arnaldigno, comanda
 
 
@@ -9,31 +9,25 @@ class Karmelo(Arnaldigno):
 
     @comanda('(.*)\+\+')
     def karmelone(self, e, match):
-        u = self.arnaldo.channels[CHAN].users()
-        icche = u'__karma_%s'.encode('utf-8') % (match.groups()[0],)
-        if match.groups()[0] in u:
-            k = brain.get(icche)
-            brain.set(icche, 1 + (int(k) if k else 0))
-            self.r(e, 'vabbé'.decode('utf-8'))
-        else:
-            self.r(e, 'nenti sacciu e nenti vitti')
+        icche = match.groups()[0].lower()
+        indove = u'__karma_%s'.encode('utf-8') % (icche,)
+        k = brain.get(indove)
+        brain.set(indove, 1 + (int(k) if k else 0))
+        self.r(e, 'vabbé'.decode('utf-8'))
 
     @comanda('(.*)\-\-')
     def karmelino(self, e, match):
-        u = self.arnaldo.channels[CHAN].users()
-        icche = u'__karma_%s'.encode('utf-8') % (match.groups()[0],)
-        if match.groups()[0] in u:
-            k = brain.get(icche)
-            brain.set(icche, (int(k) if k else 0) - 1)
-            self.r(e, 'vabbé'.decode('utf-8'))
-        else:
-            self.r(e, 'nenti sacciu e nenti vitti')
+        icche = match.groups()[0].lower()
+        indove = u'__karma_%s'.encode('utf-8') % (icche,)
+        k = brain.get(indove)
+        brain.set(indove, (int(k) if k else 0) - 1)
 
     @comanda('^%s\s*[:,]\s*(.*)\?' % (NICK, ))
     def karma(self, e, match):
-        icche = u'__karma_%s'.encode('utf-8') % (match.groups()[0],)
+        icche = match.groups()[0].lower()
+        indove = u'__karma_%s'.encode('utf-8') % (icche,)
         k = brain.get(icche)
         if k:
-            self.r(e, u'karma %s: %d'.encode('utf-8') % (match.groups()[0], int(k)))
+            self.r(e, u'karma %s: %d'.encode('utf-8') % (indove, int(k)))
         else:
-            self.r(e, u'%s chi?'.encode('utf-8') % (match.groups()[0], ))
+            self.r(e, u'%s chi?'.encode('utf-8') % (indove, ))
