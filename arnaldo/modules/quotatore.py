4667440065269c8674bb08aca84e38582aadd1a4
autoscatto <4utoscatto@gmail.com>
2016-11-11 12:33:17 +0100

    unicuore

--- arnaldo/modules/quotatore.py
+++ arnaldo/modules/quotatore.py
@@ -40,7 +40,7 @@ class Quotatore(Arnaldigno):
             return
 
         q = eval(q)
-        self.r(e, u'#%s: %s' % (q['id'], unicode(q['quote'], 'utf-8')))
+        self.r(e, u'#%s: %s' % (q['id'], q['quote']))
 
     @comanda('^%s[:, \\t]*quote #(\d+)$')
     def get_quote(self, e, match):
