4e404296dd3ade4ae86f55bb576d841c99e7ee32
leonardo <leofiore@libero.it>
2021-07-21 00:41:29 +0200

    PURE I DUE PUNTI! PURE I DUE PUNTI!

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 # SE GIRI CON ROOT SEI UN GRAN FARABOOT
 grep "${RUNAS_GID}" /etc/group > /dev/null || addgroup -g "${RUNAS_GID}" "${RUNAS_GROUP}"
-grep "${RUNAS_UID}" /etc/passwd > /dev/null || adduser -S -u "${RUNAS_UID}" "${RUNAS_USER}"
+grep "${RUNAS_UID}" /etc/passwd > /dev/null || adduser -h /opt/arnaldo -S -u "${RUNAS_UID}" "${RUNAS_USER}"
 
 # CHI NON CLONA È COMPLICE
 chown -R "${RUNAS_USER}:${RUNAS_GROUP}" .
